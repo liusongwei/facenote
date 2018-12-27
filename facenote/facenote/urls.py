596ab86165dc1333ea7f89767b7489a32f567bcd
horizonrh <horizonrh@gmail.com>
2018-12-27 20:55:24 +0800

    login app

--- facenote/facenote/urls.py
+++ facenote/facenote/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from login import views as login_views
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
+    path('login', login_views.index, name='login'),
 ]
